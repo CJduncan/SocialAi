c03d4e35c67a078d7f8d19d2ac8ce594cb757849
CJduncan <136113552+CJduncan@users.noreply.github.com>
2023-11-21 16:17:12 -0600

    files downloades as well as index created
    started local server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.get('/', async (req, res) => {
+  res.send('Hello from Dalle ');
+});
+
+const startServer = async () => {
+  
+    app.listen(8081, () => console.log('Server started on port 8081'));
+
+};
+
+startServer();
